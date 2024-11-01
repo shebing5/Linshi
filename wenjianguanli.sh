66c21f14b5296933dd39becc47136055140e120b
shebing5 <126408626+shebing5@users.noreply.github.com>
2024-11-02 01:21:06 +0800

    更新 wenjianguanli.sh

--- wenjianguanli.sh
+++ wenjianguanli.sh
@@ -47,6 +47,96 @@ function redirect_file() {
     echo "已移动文件 $file 到 $destination/$new_filename"
 }
 
+# 分类和移动文件的函数，避免重复代码
+function classify_and_move() {
+    local file="$1"
+    filename=$(basename "$file")
+    extension="${filename##*.}"
+    extension_lower=$(echo "$extension" | tr '[:upper:]' '[:lower:]')
+
+    case "$extension_lower" in
+        # 压缩包
+        zip|rar|7z|tar|gz|bz2|xz|tar.gz|tar.bz2|tar.xz)
+            destination="/storage/emulated/0/下载/压缩包/"
+            ;;
+        # 图片
+        png|jpg|jpeg|gif|bmp|tiff|svg|webp|heic|heif)
+            destination="/storage/emulated/0/下载/图片/"
+            ;;
+        # 软件
+        apk|exe|msi|deb|rpm|dmg|pkg|appimage|bat|sh)
+            destination="/storage/emulated/0/下载/软件/"
+            ;;
+        # 视频
+        mp4|avi|mov|wmv|flv|mkv|webm|vob|rmvb|3gp|ts|m4v)
+            destination="/storage/emulated/0/下载/视频/"
+            ;;
+        # 文档
+        doc|docx|pdf|xls|xlsx|ppt|pptx|odt|ods|odp|rtf)
+            destination="/storage/emulated/0/下载/文档/"
+            ;;
+        # 音频
+        mp3|wav|flac|aac|ogg|m4a|wma|alac|ape)
+            destination="/storage/emulated/0/下载/音频/"
+            ;;
+        # 文本
+        txt|log|md|json|xml|csv|yaml|yml|ini|conf|cfg)
+            destination="/storage/emulated/0/下载/文本/"
+            ;;
+        # 脚本和代码
+        sh|py|js|html|htm|css|php|java|c|cpp|h|hpp|go|rb|pl|lua|swift|kt|cs|ts|sql|R|r|ipynb)
+            destination="/storage/emulated/0/下载/代码/"
+            ;;
+        # 磁力链接文件
+        torrent|magnet)
+            destination="/storage/emulated/0/下载/种子/"
+            ;;
+        # 光盘镜像
+        iso|img|bin|cue|nrg|mdf|mds|dmg)
+            destination="/storage/emulated/0/下载/镜像文件/"
+            ;;
+        # 字体文件
+        ttf|otf|fon|woff|woff2|eot)
+            destination="/storage/emulated/0/下载/字体/"
+            ;;
+        # 数据库文件
+        db|sql|sqlite|mdb|accdb|dbf)
+            destination="/storage/emulated/0/下载/数据库/"
+            ;;
+        # 电子书
+        epub|mobi|azw3|djvu|fb2|ibooks|chm)
+            destination="/storage/emulated/0/下载/电子书/"
+            ;;
+        # 压缩音乐
+        zipx|rar|7z|flac|ape)
+            destination="/storage/emulated/0/下载/无损音乐/"
+            ;;
+        # PSD 文件
+        psd|ai|eps|indd)
+            destination="/storage/emulated/0/下载/设计文件/"
+            ;;
+        # 应用数据
+        plist|mobileconfig|cer|crt|pem|der)
+            destination="/storage/emulated/0/下载/应用数据/"
+            ;;
+        # 备份文件
+        bak|backup|bk)
+            destination="/storage/emulated/0/下载/备份文件/"
+            ;;
+        # 临时文件
+        tmp|temp|swp|swo|dmp)
+            destination="/storage/emulated/0/下载/临时文件/"
+            ;;
+        # 其他
+        *)
+            destination="/storage/emulated/0/下载/其他/"
+            ;;
+    esac
+
+    check_dir "$destination"
+    redirect_file "$file" "$destination"
+}
+
 # 移动文件并分类
 function move_files() {
     local src_dir="$1"
@@ -57,91 +147,7 @@ function move_files() {
     fi
 
     find "$src_dir" -maxdepth 1 -type f | while read -r file; do
-        filename=$(basename "$file")
-        extension="${filename##*.}"
-        extension_lower=$(echo "$extension" | tr '[:upper:]' '[:lower:]')
-
-        case "$extension_lower" in
-            # 压缩包
-            zip|rar|7z|tar|gz|bz2|xz|tar.gz|tar.bz2|tar.xz)
-                destination="/storage/emulated/0/下载/压缩包/"
-                ;;
-            # 图片
-            png|jpg|jpeg|gif|bmp|tiff|svg|webp|heic|heif)
-                destination="/storage/emulated/0/下载/图片/"
-                ;;
-            # 软件
-            apk|exe|msi|deb|rpm|dmg|pkg|appimage|bat|sh)
-                destination="/storage/emulated/0/下载/软件/"
-                ;;
-            # 视频
-            mp4|avi|mov|wmv|flv|mkv|webm|vob|rmvb|3gp|ts|m4v)
-                destination="/storage/emulated/0/下载/视频/"
-                ;;
-            # 文档
-            doc|docx|pdf|xls|xlsx|ppt|pptx|odt|ods|odp|rtf)
-                destination="/storage/emulated/0/下载/文档/"
-                ;;
-            # 音频
-            mp3|wav|flac|aac|ogg|m4a|wma|alac|ape)
-                destination="/storage/emulated/0/下载/音频/"
-                ;;
-            # 文本
-            txt|log|md|json|xml|csv|yaml|yml|ini|conf|cfg)
-                destination="/storage/emulated/0/下载/文本/"
-                ;;
-            # 脚本和代码
-            sh|py|js|html|htm|css|php|java|c|cpp|h|hpp|go|rb|pl|lua|swift|kt|cs|ts|sql|R|r|ipynb)
-                destination="/storage/emulated/0/下载/代码/"
-                ;;
-            # 磁力链接文件
-            torrent|magnet)
-                destination="/storage/emulated/0/下载/种子/"
-                ;;
-            # 光盘镜像
-            iso|img|bin|cue|nrg|mdf|mds|dmg)
-                destination="/storage/emulated/0/下载/镜像文件/"
-                ;;
-            # 字体文件
-            ttf|otf|fon|woff|woff2|eot)
-                destination="/storage/emulated/0/下载/字体/"
-                ;;
-            # 数据库文件
-            db|sql|sqlite|mdb|accdb|dbf)
-                destination="/storage/emulated/0/下载/数据库/"
-                ;;
-            # 电子书
-            epub|mobi|azw3|djvu|fb2|ibooks|chm)
-                destination="/storage/emulated/0/下载/电子书/"
-                ;;
-            # 压缩音乐
-            zipx|rar|7z|flac|ape)
-                destination="/storage/emulated/0/下载/无损音乐/"
-                ;;
-            # PSD 文件
-            psd|ai|eps|indd)
-                destination="/storage/emulated/0/下载/设计文件/"
-                ;;
-            # 应用数据
-            plist|mobileconfig|cer|crt|pem|der)
-                destination="/storage/emulated/0/下载/应用数据/"
-                ;;
-            # 备份文件
-            bak|backup|bk)
-                destination="/storage/emulated/0/下载/备份文件/"
-                ;;
-            # 临时文件
-            tmp|temp|swp|swo|dmp)
-                destination="/storage/emulated/0/下载/临时文件/"
-                ;;
-            # 其他
-            *)
-                destination="/storage/emulated/0/下载/其他/"
-                ;;
-        esac
-
-        check_dir "$destination"
-        redirect_file "$file" "$destination"
+        classify_and_move "$file"
     done
 }
 
@@ -175,6 +181,8 @@ paths=(
     "/storage/emulated/0/Android/data/com.coolapk.market/files/Download/"
     "/storage/emulated/0/Android/data/tw.nekomimi.nekogram/files/Telegram/Telegram Files/"
     # 新增路径，可以在此添加
+    "/storage/emulated/0/Download/ExtraPath1/"
+    "/storage/emulated/0/Download/ExtraPath2/"
 )
 
 # 处理每个路径
@@ -192,32 +200,8 @@ function move_files_recursive() {
     fi
 
     find "$parent_dir" -type f | while read -r file; do
-        # 与 move_files 函数中的处理方式相同
-        filename=$(basename "$file")
-        extension="${filename##*.}"
-        extension_lower=$(echo "$extension" | tr '[:upper:]' '[:lower:]')
-
-        # 重复使用之前的分类逻辑
-        # ...（这里可重复使用之前的 case 语句）
-
-        # 为了避免重复代码，可以将分类逻辑封装成一个函数
         classify_and_move "$file"
     done
-}
-
-# 分类和移动文件的函数，避免重复代码
-function classify_and_move() {
-    local file="$1"
-    filename=$(basename "$file")
-    extension="${filename##*.}"
-    extension_lower=$(echo "$extension" | tr '[:upper:]' '[:lower:]')
-
-    case "$extension_lower" in
-        # ...（与之前的 case 语句相同）
-    esac
-
-    check_dir "$destination"
-    redirect_file "$file" "$destination"
 }
 
 # 对指定目录进行递归处理
